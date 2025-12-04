757f44a308d60fb2fd3c828c592885fdf600d304
I.Hara <i.hara.git@gmail.com>
2025-12-04 11:55:45 +0900

    Finish lesson 9 code

---
+++ mypkg/talker.py
@@ -0,0 +1,18 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+rclpy.init()
+node = Node("talker")
+
+def cb(request, response):
+    if request.name == "原一颯":
+        response.age = 20
+    else:
+        response.age = 255
+    
+    return response
+
+def main():
+    srv = node.create_service(Query, "query", cb)
+    rclpy.spin(node)
